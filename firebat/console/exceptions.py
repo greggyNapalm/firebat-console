440e928493695ea443c8bffe7198427e28b46bc3
Gregory Komissarov <gregory.komissarov@gmail.com>
2012-06-18 14:52:08 +0400

    istepper_init: forget to add exceptions file

---
+++ firebat/console/exceptions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+firebat.console.exeptions
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Exception classes related with config files
+"""
+
+import string
+import logging
+
+class StepperAmmoFormat(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        #return repr(self.value)
+        return self.value
+
+class StepperSchemaFormat(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        #return repr(self.value)
+        return self.value
