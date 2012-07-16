b3c5a8b72c5c47d93f9bd99e9f3acef0ec3b31a6
Gregory Komissarov <gregory.komissarov@gmail.com>
2012-07-16 11:59:34 +0400

    some charts added

--- firebat/console/aggr.py
+++ firebat/console/aggr.py
@@ -16,6 +16,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler as rh
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+import numpy
 import simplejson as json
 from simplejson.decoder import JSONDecodeError
 
@@ -64,18 +65,33 @@ class phout_stat(object):
         self.responses_num = 0.0
         self.http_codes_num = 0.0
         self.reply_series = {
-            'name': 'rps',
+            'name': 'replies',
             'data': [],
         }
         self.resp_perc = []
+        self.total_tx = 0.0
+        self.total_rx = 0.0
+        self.tx_series = {'name': 'tx', 'data': [], }
+        self.rx_series = {'name': 'rx', 'data': [], }
+        self.rtt_fracts = ['con_ms', 'send_ms', 'proc_ms', 'resp_ms']
+        self.rtt_fracts_series = {}
+        for part in self.rtt_fracts:
+            self.rtt_fracts_series[part] = {'name': part, 'data': [], }
 
-    def add_resp(self, epoch, rtt, http_status, errno):
+    def add_resp(self, epoch, rtt, http_status, errno, req_byte, resp_byte,
+                 con_ms, send_ms, proc_ms, resp_ms):
         '''Process regular log line.
         Args:
             epoch: int, time stamp.
             rtt: int, request round trip time.
             http_status: int, responce HTTP status code.
             errno: str, errno code from TCP socket.
+            req_byte: int, request size in bytes.
+            resp_byte: int, responce size in bytes.
+            con_ms: float, TCP connection establishing time in milliseconds.
+            send_ms: float, request sending time in milliseconds.
+            proc_ms: float, awaiting responce time in milliseconds.
+            resp_ms: float, getting responce time in milliseconds.
 
         Returns:
             nothing, just update obj attributes
@@ -130,6 +146,24 @@ class phout_stat(object):
         except KeyError:
             self.errno_tbl[errno] = {'num': 1}
 
+        # Tx/Rx bytes processing for each req
+        try:
+            self.resp[epoch]['tx'] += req_byte
+            self.resp[epoch]['rx'] += resp_byte
+        except KeyError:
+            self.resp[epoch]['tx'] = req_byte
+            self.resp[epoch]['rx'] = resp_byte
+
+        # rtt fractions for each req
+        if not 'rtt_fract' in self.resp[epoch]:
+            self.resp[epoch]['rtt_fract'] = {}
+
+        for part in self.rtt_fracts:
+            try:
+                self.resp[epoch]['rtt_fract'][part].append(vars()[part])
+            except KeyError:
+                self.resp[epoch]['rtt_fract'][part] = [vars()[part], ]
+
     def calc_percentiles(self, scrend_out_stmps):
         '''Aggregate added responces data.
             * resp time percentiles
@@ -149,7 +183,7 @@ class phout_stat(object):
         for e in self.errno_lst:
             self.errno_series[e] = []
         for tick, r in self.resp.iteritems():
-            if tick in scrend_out_stmps:
+            if tick in scrend_out_stmps:  # calc stats not for all points
                 # responce time calc
                 r['rtt'].sort()
                 r['rps'] = len(r['rtt'])
@@ -176,6 +210,17 @@ class phout_stat(object):
                                                      r['errno'].get(e, 0)))
                     except KeyError:
                         self.errno_series[e] = [(tick, r['errno'].get(e, 0))]
+
+                # tx/rx
+                self.tx_series['data'].append((tick, r['tx']))
+                self.rx_series['data'].append((tick, r['rx']))
+                self.total_tx += r['tx']
+                self.total_rx += r['rx']
+
+                # rtt parts
+                for part in self.rtt_fracts:
+                    mean = numpy.mean(r['rtt_fract'][part])
+                    self.rtt_fracts_series[part]['data'].append((tick, mean))
 
     def get_errno_hcds(self):
         '''Make highcharts data series for errno chart.
@@ -359,10 +404,9 @@ def output_data(stat, calc_load_series, series_path='data_series.js'):
     #for i in range (2):
     #    print 'errno%s: %s' % (i, errno[i]['data'][num][0])
 
+    # d1
     with open(series_path, 'w+') as ds_fh:
         ds_fh.write('rps_series = ' + json.dumps(calc_load_series) + ';\n')
-        #ds_fh.write('reply_series = ' + json.dumps(rps_series,
-        #    indent=4 * ' ') + ';\n')
         ds_fh.write('resp_percentiles_series = ' +\
                 json.dumps(stat.get_resp_perc_hcds()) + ';\n')
         ds_fh.write('status_codes_series = ' +\
@@ -370,8 +414,25 @@ def output_data(stat, calc_load_series, series_path='data_series.js'):
         ds_fh.write('errno_series = ' +\
                 json.dumps(stat.get_errno_hcds()) + ';\n')
 
+    with open(series_path.replace('.js', '1.js'), 'w+') as ds1_fh:
+        ds1_fh.write('rps_series = ' + json.dumps(calc_load_series) + ';\n')
+        ds1_fh.write('reply_series = ' + json.dumps(stat.reply_series) + ';\n')
+        ds1_fh.write('tx_series = ' + json.dumps(stat.tx_series) + ';\n')
+        ds1_fh.write('rx_series = ' + json.dumps(stat.rx_series) + ';\n')
+        # rtt parts
+        for part in stat.rtt_fracts:
+            ds1_fh.write('%s_series = ' % part +\
+                json.dumps(stat.rtt_fracts_series[part]) + ';\n')
+
 
 def get_pages_context(stat, fire):
+    ''' Create dict with data to render Web pages templates.
+    Args:
+        stat: phout_stat class instance.
+        fire: dict, fire data.
+    Returns:
+        ctx: dict, result dict used by Jinja2 template engine.
+    '''
     ctx = {}
     ctx['tgt_addr'] = fire.get('addr')
     ctx['load'] = fire['load']
@@ -418,7 +479,6 @@ def process_phout(phout_fh, points_num=200, dst_file='data_series.js',
     Returns:
         static Web app on file system.
     '''
-
     fire = get_fire(json_path=fire_path)
     calc_load_series = {  # rps calculated data series
         'name': 'rps',
@@ -439,7 +499,7 @@ def process_phout(phout_fh, points_num=200, dst_file='data_series.js',
         # http://phantom-doc-ru.rtfd.org/en/latest/analyzing_result_data.html
         if len(l_spltd) != 12:
             print 'Malformed line in phout file: %s' % l
-        epoch, tag, rtt, con_ms, send_ms, proc_ms, resp_ms, phantom_exec, \
+        epoch, tag, rtt, con_mcs, send_mcs, proc_mcs, resp_mcs, phantom_exec, \
             req_byte, resp_byte, errno, http_status = l_spltd
         epoch = int(epoch.split('.')[0])  # cut out fractional part of epoach
         if epoch > current_epoch:
@@ -447,7 +507,10 @@ def process_phout(phout_fh, points_num=200, dst_file='data_series.js',
                 p_stat.first_epoach = epoch
             current_epoch = epoch
             p_stat.last_epoach = epoch
-        p_stat.add_resp(int(current_epoch), int(rtt), int(http_status), errno)
+        p_stat.add_resp(int(current_epoch), int(rtt), int(http_status), errno,
+                        int(req_byte), int(resp_byte), float(con_mcs) / 1000,
+                        float(send_mcs) / 1000, float(proc_mcs) / 1000,
+                        float(resp_mcs) / 1000)
 
     # all phout lines parsed, time to aggregate data to expected metrics
     p_stat.calc_percentiles(scrend_out_stmps)
